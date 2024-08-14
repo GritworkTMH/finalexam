370454e34769913b1bd226258de5d539f1573096
Thet Myat Hnin <142704734+GritworkTMH@users.noreply.github.com>
2024-08-14 16:00:48 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Test</title>
+    </head>
+    <body>
+        <?php echo '<p>Hello World from PHP</p>'; ?>
+        <?php echo '<p>Hello World from HNIN</p>'; ?>
+    </body>
+</html>
